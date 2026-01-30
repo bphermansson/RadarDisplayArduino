933bf4f37e9673c4be76743c0ca633e5486e703b
Patrik Hermansson <hermansson.patrik@gmail.com>
2026-01-30 12:28:39 +0100

    .

--- src/AppSettings.cpp
+++
@@ -1,7 +0,0 @@
-#include "AppSettings.h"
-const char* smhi_temp_url =
-    "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/12.680/lat/58.333/data.json";
-const char* smhi_forecast_url =
-    "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/13/station/82260/period/latest-hour/data.json";
-const char* ssid = "BrandstorpWifi";
-const char* password = "Brandstorp";
